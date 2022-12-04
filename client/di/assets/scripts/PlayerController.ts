7aba9b37782341ca461e41a92ea6b39085b403cd
zuan <sevenneil@foxmail.com>
2022-12-04 15:16:33 +0800

    change map & change camera color & fix some little bug“

--- client/di/assets/scripts/PlayerController.ts
+++ client/di/assets/scripts/PlayerController.ts
@@ -11,7 +11,6 @@ export class PlayerController extends Component {
 
     start() {
         let collider = this.node.getComponent(BoxCollider2D);
-        console.info('player layer grou[', collider.group);
     }
 
     update(deltaTime: number) {
@@ -27,32 +26,25 @@ export class PlayerController extends Component {
     }
 
     onMouseUp(event: EventMouse) {
-        console.info(event.getType())
-        console.info('ddddddddddd')
-        console.info(event.getButton())
     }
 
     getPlayerControl(event: EventKeyboard){
         if (event.keyCode == KeyCode.KEY_A || event.keyCode == KeyCode.ARROW_LEFT){
-            console.info('lleft');
             this.Move(-this._speed, 0); 
             this.setState('player_left');
         }
 
         if (event.keyCode == KeyCode.KEY_D || event.keyCode ==KeyCode.ARROW_RIGHT){
-            console.info('rrright');
             this.Move(this._speed, 0); 
             this.setState('player_right');
         }
 
         if (event.keyCode == KeyCode.KEY_W || event.keyCode == KeyCode.ARROW_UP){
-            console.info('uuup');
             this.Move(0, this._speed); 
             this.setState('player_up');
         }
 
         if (event.keyCode == KeyCode.KEY_S || event.keyCode == KeyCode.ARROW_DOWN){
-            console.info('ddddown');
             this.Move(0, -this._speed); 
             this.setState('player_down');
         }
@@ -73,58 +65,6 @@ export class PlayerController extends Component {
 
     onKeyUp(event: EventKeyboard){
         
-//event.keyCode
-
-        // switch (event.keyCode) {
-        //     case KeyCode.KEY_A || KeyCode.ARROW_LEFT:
-        //         console.info('lleft');
-        //         move = new math.Vec2(-this._speed, 0);
-        //         break; 
-        //     case KeyCode.KEY_D || KeyCode.ARROW_RIGHT:
-        //         console.info('lleft');
-        //         move = new math.Vec2(-this._speed, 0);
-        //         break; 
-        //     case KeyCode.KEY_W || KeyCode.ARROW_UP:
-        //         console.info('lleft');
-        //         move = new math.Vec2(-this._speed, 0);
-        //         break; 
-        //     case KeyCode.KEY_S || KeyCode.ARROW_DOWN:
-        //         console.info('lleft');
-        //         move = new math.Vec2(-this._speed, 0);
-        //         break; 
-        // }
-
-       
-
-        // if (Input[KeyCode.KEY_A || KeyCode.ARROW_LEFT]){
-        //     console.info('lleft');
-        //     move = new math.Vec2(-this._speed, 0); 
-        // }
-
-        // if (Input[KeyCode.KEY_D || KeyCode.ARROW_RIGHT]){
-        //     console.info('rrright');
-        //     move = new math.Vec2(this._speed, 0); 
-        // }
-
-        // if (Input[KeyCode.KEY_W || KeyCode.ARROW_UP]){
-        //     console.info('uuup');
-        //     move = new math.Vec2(0, this._speed); 
-        // }
-
-        // if (Input[KeyCode.KEY_S || KeyCode.ARROW_DOWN]){
-        //     console.info('ddddown');
-        //     move = new math.Vec2(0, -this._speed); 
-        // }
-
-        
-
-        
-        //Input[event.keyCode] = false;
-
-        // console.info(event.getType())
-        // console.info('key up')
-        // console.info(event.keyCode)
-        // console.info(event.isPressed)
     }
 
     onKeyPressing(event: EventKeyboard){
--- client/di/assets/scripts/game.ts
+++ client/di/assets/scripts/game.ts
@@ -1,11 +1,11 @@
-import { _decorator, Component, Node, TiledMap, RigidBody2D, PhysicsSystem2D, ERigidBody2DType, Vec2, BoxCollider2D, UITransform } from 'cc';
+import { _decorator, Component, Node, TiledMap, RigidBody2D, PhysicsSystem2D, ERigidBody2DType, Vec2, BoxCollider2D, UITransform, resources, TiledMapAsset, Vec3, MouseJoint2D } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('game')
 export class game extends Component {
 
     @property({type: Node})
-    public map: Node | null = null;
+    public mapNode: Node | null = null;
 
     public static physicGroupType = {
         wall : 1 << 1,
@@ -13,41 +13,76 @@ export class game extends Component {
         smog: 1 << 3
     };
 
-    start() {
-        let p = PhysicsSystem2D.instance;
-        p.debugDrawFlags = 1;
+    initMapNode(mapNode) {
+        if (mapNode){
+            mapNode.getComponent(UITransform).setAnchorPoint(new Vec2(0, 0));
 
-        if (this.map){
-            for (const mapNode of this.map.children) {
-                mapNode.getComponent(UITransform).setAnchorPoint(new Vec2(0, 0));
+            let map = mapNode.getComponent(TiledMap);
+            map.enableCulling = false;
 
-                let map = mapNode.getComponent(TiledMap);
-                let tiledSize = map.getTileSize();
-                console.info("tiledSize", tiledSize)
-                
-                let wallLayer = map.getLayer('wall');
-                let wallLayerSize = wallLayer.getLayerSize();
+            let tiledSize = map.getTileSize();
+            
+            let wallLayer = map.getLayer('wall');
+            let wallLayerSize = wallLayer.getLayerSize();
 
 
-                for (let i = 0; i < wallLayerSize.width; i++) {
-                    for (let j = 0; j < wallLayerSize.height; j++) {
-                        let tiled = wallLayer.getTiledTileAt(i, j, true);
-                        let tiledId = tiled.grid;
-                        
-                        if (tiledId != 0) {
-                            let body = tiled.node.addComponent(RigidBody2D);
-                            body.type = ERigidBody2DType.Static;
-                            body.group = game.physicGroupType.wall;
-                            let collider = tiled.node.addComponent(BoxCollider2D);
-                            collider.offset = new Vec2(tiledSize.width/2, tiledSize.height/2);
-                            collider.size = tiledSize;
-                            collider.group = game.physicGroupType.wall;
-                            collider.apply();
-                        }
+            for (let i = 0; i < wallLayerSize.width; i++) {
+                for (let j = 0; j < wallLayerSize.height; j++) {
+                    let tiled = wallLayer.getTiledTileAt(i, j, true);
+                    let tiledId = tiled.grid;
+                    
+                    if (tiledId != 0) {
+                        let body = tiled.node.addComponent(RigidBody2D);
+                        body.type = ERigidBody2DType.Static;
+                        body.group = game.physicGroupType.wall;
+                        let collider = tiled.node.addComponent(BoxCollider2D);
+                        collider.offset = new Vec2(tiledSize.width/2, tiledSize.height/2);
+                        collider.size = tiledSize;
+                        collider.group = game.physicGroupType.wall;
+                        collider.apply();
                     }
                 }
             }
         }
+    }
+
+    init(mapNameArr) {
+        for (let index = 0; index < mapNameArr.length; index++) {
+            for (let j = 0; j < mapNameArr[index].length; j++) {
+                const element = mapNameArr[index][j];
+
+                if (!element || element == '00000') continue;
+
+                resources.load(`maps/${element}`, TiledMapAsset, (err, asset) => {
+                    let node = new Node();
+                    let map = node.addComponent(TiledMap);
+
+                    let x = (j - Math.floor(mapNameArr[index].length / 2)) * 384;
+                    let y = -(index - Math.floor(mapNameArr[index].length / 2)) * 384;
+                    node.setPosition(new Vec3(x, y ,0));
+                    
+                    map.tmxAsset = asset;
+                    node.parent = this.mapNode;
+                    this.initMapNode(node);
+                })
+            }
+            
+        }
+    }
+
+    onLoad() {
+        let mapNameArr = [
+            ['00000', '01000', '00000'],
+            ['00010', '11110', '00100'],
+            ['00000', '10000', '00000'],
+        ]
+
+        this.init(mapNameArr);
+    }
+
+    start() {
+        // let p = PhysicsSystem2D.instance;
+        // p.debugDrawFlags = 1;
     }
 
     update(deltaTime: number) {
