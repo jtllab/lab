8a053926db7efe9adb09a50acec9a34a05c9d0ac
HGD <370561786@qq.com>
2022-12-04 10:44:52 +0800

    添加子弹

--- client/LegendOfArrow/assets/script/bulletControl.ts
+++ client/LegendOfArrow/assets/script/bulletControl.ts
@@ -1,37 +1,41 @@
-import { _decorator, Component, Node, Vec2, NodePool, SpriteFrame, Sprite, Input, input } from 'cc';
+import { _decorator, Component, Node, Vec2, NodePool, SpriteFrame, Sprite, Input, input, math, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('bullet')
-export class bullet extends Component {
+export class bulletControl extends Component {
 
+ // 子弹的移动方向
+ public posOffset: math.Vec3 = new math.Vec3(0, 0, 0);
 
-    onLoad(){
-      this.schedule(this.onTimer,0.01);
-    }
+ // 记录子弹的初始位置，用于检查子弹是否飞出太远，如果太远则销毁子弹，防止子弹一直存在浪费计算机资源
+ private beginPos: Vec3;
+ start() {
+     this.beginPos = this.node.getPosition();
+ }
 
-    start() {
+ onLoad() {
+     // 每 1/60 秒调用一次 onTimer 函数
+     this.schedule(this.onTimer, 1 / 60);
+ }
 
-    }
+ // 由于我们这里使用定时器来更新子弹飞行位置，所以不需要在 update 函数中更新子弹位置
+ update(deltaTime: number) {}
 
-    update(deltaTime: number) {
-
-    }
-    
-  
-
-
-
-  onTimer(){
-      if(this.node.getPosition().y>300){
-          this.unschedule(this.onTimer);
-          this.node.destroy();
-          return;
-      }
-      this.node.getPosition().y+=5;
-  }
-
-
-
+ // 定时器回调函数，更新子弹位置
+ onTimer() {
+     {
+         // 检查子弹是否飞出太远，如果是则销毁子弹，防止一直存在浪费计算机资源
+         if (this.node.getPosition().subtract(this.beginPos).length() > 1000) {
+             // 取消这个定时器，防止一直存在浪费计算机资源
+             this.unschedule(this.onTimer);
+             // 销毁这个node就是销毁这个子弹
+             this.node.destroy();
+             return;
+         }
+     }
+     // 更新子弹位置
+     this.node.setPosition(this.node.getPosition().add(this.posOffset));
+ }
 }
 
 
--- client/LegendOfArrow/assets/script/heroControl.ts
+++ client/LegendOfArrow/assets/script/heroControl.ts
@@ -1,5 +1,6 @@
-import { _decorator, Component, Node, input, Input, EventKeyboard, KeyCode,Animation, Sprite, SpriteFrame, instantiate, Prefab } from 'cc';
+import { _decorator, Component, Node, input, Input, EventKeyboard, KeyCode,Animation, Sprite, SpriteFrame, instantiate, Prefab, math } from 'cc';
 const { ccclass, property } = _decorator;
+import { bulletControl }  from './bulletControl';
 
 @ccclass('heroControl')
 export class heroControl extends Component {
@@ -19,6 +20,12 @@ export class heroControl extends Component {
     //hero移动速度
     speed:number = 2;
 
+    // hero移动单位偏移量，每次update时，英雄移动的偏移量等于 posOffset * posOffsetMul，posOffsetMul为0时，不移动。
+    posOffsetMul: number = 0;
+    // 保留 posOffset 的原因是这相当于是子弹射出方向，子弹的移动方向也是根据这个来的
+    posOffset: math.Vec3 = new math.Vec3(0, -this.speed, 0);
+
+    
     @property(SpriteFrame)
     bulleteicon : SpriteFrame = null;
 
@@ -47,7 +54,7 @@ export class heroControl extends Component {
 
     update(deltaTime: number) {
         
-        if(this.left)
+      /*  if(this.left)
         {
             if(this.up)
             {
@@ -91,6 +98,48 @@ export class heroControl extends Component {
             //节点y坐标负方向移动
             this.node.setPosition(this.node.getPosition().x,this.node.getPosition().y-this.speed);
 
+        }*/
+
+        // 注意，因为 Vec3 的计算方法都会修改自己的值，所以要先 clone 一个值再操作，避免修改到原始值
+        var off = this.posOffset.clone();
+        // 乘上 posOffsetMul，在不移动时，这个值为 0，乘以0后这个向量就是 0 了，就不会移动了
+        off = off.multiplyScalar(this.posOffsetMul);
+        // 用位置偏移量更新节点位置
+        this.node.setPosition(this.node.getPosition().add(off));
+
+    }
+    updatePosOffset() {
+        this.posOffsetMul = 0;
+        if (this.left || this.right || this.up || this.down) {
+            // 如果有按键按下，就把速度设置乘数设置为1，这样就会移动了
+            this.posOffsetMul = 1;
+
+            // 根据按键情况更新方向
+            // 这里有一个 BUG，如果同时按下横竖两个方向键，会导致英雄实际跑动方向更快，就留给你们自己修复了(提示，斜向走时，减小 posOffsetMul 的值)
+            if (this.left) {
+                //节点X坐标正方向移动
+                this.posOffset.x = -this.speed;
+                this.setState("hero_left");
+            } else if (this.right) {
+                //节点X坐标负方向移动
+                this.posOffset.x = this.speed;
+                this.setState("hero_right");
+            } else {
+                this.posOffset.x = 0;
+            }
+            if (this.up) {
+                //节点y坐标正方向移动
+                this.posOffset.y = this.speed;
+                this.setState("hero_up");
+            } else if (this.down) {
+                //节点y坐标负方向移动
+                this.posOffset.y = -this.speed;
+                this.setState("hero_down");
+            } else {
+                this.posOffset.y = 0;
+            }
+        } else {
+            this.posOffsetMul = 0;
         }
     }
 
@@ -125,6 +174,8 @@ export class heroControl extends Component {
                 this.setState('hero_down');
                 break;
         }
+        this.updatePosOffset();
+
     }
     //按键起升时触发
     onKeyUp(enevt:EventKeyboard)
@@ -144,6 +195,8 @@ export class heroControl extends Component {
                 this.down = false;
                 break;
         }
+        this.updatePosOffset();
+
     }
 
     setState(state){
@@ -168,12 +221,19 @@ export class heroControl extends Component {
         let bullet:Node = instantiate(this.bulletPrefab);
         
         this.node.parent.addChild(bullet);
-
-       
+        // 注意，因为 Vec3 的计算方法都会修改自己的值，所以要先 clone 一个值再操作，避免修改到原始值
+        var posOffset = this.posOffset.clone();
         //子弹图层设置等于父节点图层
-        bullet.layer = this.node.layer;
+       // bullet.layer = this.node.layer;
         //设置相对父节点位置
         bullet.setPosition(this.node.position);
+
+        
+        // 初始化子弹的移动速度，这包括的是子弹的方向和速度
+        bullet.getComponent(bulletControl).posOffset = posOffset.multiplyScalar(5);
+
+        //挂载到炮台节点下
+        this.node.parent.addChild(bullet);
     }
 
 
