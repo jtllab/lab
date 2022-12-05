f483f16661fa207724332caeea17a40b46c7f2d4
HGD <370561786@qq.com>
2022-12-05 11:54:53 +0800

    添加子弹碰撞和HP，暂为实现HP减少

---
+++ client/LegendOfArrow/assets/script/hitControl.ts
@@ -0,0 +1,18 @@
+import { _decorator, Component, Node, director, PhysicsSystem2D, Contact2DType, Collider2D, IPhysics2DContact, ProgressBar } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('mainControl')
+export class mainControl extends Component {
+
+  
+
+    start() {
+        
+    }
+
+    update(deltaTime: number) {
+        
+    }
+  
+    
+}
---
+++ client/LegendOfArrow/assets/script/slfsControl.ts
@@ -0,0 +1,44 @@
+import { _decorator, Component, Node, ProgressBar, PhysicsSystem2D, Contact2DType, Collider2D, IPhysics2DContact } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('slfsControl')
+export class slfsControl extends Component {
+    @property(ProgressBar)
+    hp_bar : ProgressBar = null;
+
+    //最大血量
+    max_hp : number = 300;
+    //当前血量
+    hp : number = 300;
+
+    onLoad()
+    {
+        if (PhysicsSystem2D.instance) {
+            PhysicsSystem2D.instance.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+        }
+    }
+    start() {
+        this.hp_bar.progress = this.hp/this.max_hp;
+    }
+
+    update(deltaTime: number) {
+        
+    }
+
+    onBeginContact(selfCollider : Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact| null)
+    {
+        // 只在两个碰撞体开始接触时被调用一次
+        //console.log('HIT');
+        switch(otherCollider.node.name){
+            case 'bullet':
+                console.log('HIT');
+                //this.hp=this.hp-10;
+                console.log('xxx');
+                console.log(this.hp)
+                this.hp_bar.progress = this.hp/this.max_hp;
+                break;
+        }
+    }
+}
+
+
