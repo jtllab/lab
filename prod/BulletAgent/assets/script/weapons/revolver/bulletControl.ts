2de482cf8f1fc3c2643317115de7db333d938b5b
SharkStream <395927418@qq.com>
2022-12-20 11:16:11 +0800

    hero can use revolver shot

---
+++ prod/BulletAgent/assets/script/weapons/revolver/bulletControl.ts
@@ -0,0 +1,113 @@
+import { _decorator, Component, Node, math, Vec3, Collider2D, IPhysics2DContact, Contact2DType, Animation, CircleCollider2D, Prefab} from 'cc';
+import { instantiate } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('bullet')
+export class bulletControl extends Component {
+
+    // 子弹的移动方向
+    public posOffset: math.Vec3 = new math.Vec3(0, 0, 0);
+
+    // 记录子弹的初始位置，用于检查子弹是否飞出太远，如果太远则销毁子弹，防止子弹一直存在浪费计算机资源
+    private beginPos: Vec3;
+
+    private _state : string = '';
+    private _playerAni: Animation = null;
+
+    collider: CircleCollider2D;
+
+    //子弹伤害
+    damage: number = 2;
+
+    @property(Prefab)
+    exp1Prefab : Prefab = null;
+ 
+    @property(Animation)
+    animation: Animation = null;
+
+    start() {
+        this.beginPos = this.node.getPosition();
+    }
+
+    onLoad() {
+        // 每 1/60 秒调用一次 onTimer 函数
+        this.schedule(this.onTimer, 1/60);
+        this._playerAni = this.node.getComponent(Animation);
+        this.collider = this.node.getComponent(CircleCollider2D);
+        this.collider.on(Contact2DType.BEGIN_CONTACT,this.onHitBegin,this);
+        
+    }
+
+    // 由于我们这里使用定时器来更新子弹飞行位置，所以不需要在 update 函数中更新子弹位置
+    update(deltaTime: number) {}
+
+    // 定时器回调函数，更新子弹位置
+    onTimer() {
+        {
+            // 检查子弹是否飞出太远，如果是则销毁子弹，防止一直存在浪费计算机资源
+            if (this.node.getPosition().subtract(this.beginPos).length() > 1000) {
+                // 取消这个定时器，防止一直存在浪费计算机资源
+                this.unschedule(this.onTimer);
+                //this.setState('exp1');
+                // 销毁这个node就是销毁这个子弹
+                this.node.destroy();
+                return;
+            }
+        }
+        // 更新子弹位置
+        this.node.setPosition(this.node.getPosition().add(this.posOffset));
+    }
+
+    onHitBegin(self: Collider2D, other: Collider2D,contact: IPhysics2DContact | null){
+        // console.log("hit begin self is:",self);
+        switch (other.node.name){
+            case "chiken":
+                // console.log("hit begin other is:",other);
+                // other.node.hp -= this.damage
+                // console.log(other.node.hp);
+                // //延时0.01s后销毁子弹
+                // this.scheduleOnce(() => {
+                //      this.node.destroy();// Code to be executed after the delay
+                // }, 0.1);  
+                // if (other.node.hp <= 0) {
+                //     console.log("chicken destory");
+                //     //延时0.1s后销毁鸡
+                //     this.scheduleOnce(() => {
+                //         if (other.node){
+                //             this.generateExp(other.node)
+                //             console.log("chicken die die die");
+
+                //             other.node.destroy();// Code to be executed after the delay
+                //         }
+                //     }, 0.01);
+                //     this.enemyBorn.chikenDied();
+                // }
+                // break;
+        }
+    }
+
+    
+    //子弹爆炸效果
+
+    setState(state){
+    if (this._state == state) return;
+
+        this._state = state;
+        if (this._playerAni){
+            this._playerAni.play(this._state);
+        }
+    // this.node.destroy();
+        
+    }
+
+    generateExp(enemyNode: Node) {
+        let exp:Node = null;
+        if (enemyNode.name == 'chiken'){
+            exp = instantiate(this.exp1Prefab);
+        }
+
+        this.node.parent.addChild(exp);
+        exp.setPosition(enemyNode.getPosition())
+    }
+}
+
---
+++ prod/BulletAgent/assets/script/weapons/weaponControl.ts
@@ -0,0 +1,69 @@
+import { _decorator, Component, Node, Prefab, instantiate, math } from 'cc';
+const { ccclass, property } = _decorator;
+import { bulletControl } from './revolver/bulletControl';
+import { heroControl } from '../hero/heroControl';
+
+@ccclass('weaponControl')
+export class weaponControl extends Component {
+    speed:number = 2;
+    posOffset: math.Vec3 = new math.Vec3(0, -this.speed, 0);
+
+    //攻击间隔时间
+    private _interval: number = 0.5;
+
+    @property(Prefab)
+    bulletPrefab : Prefab = null;
+
+    @property(Prefab)
+    revolverPrefab : Prefab = null;
+
+    onLoad(){
+        this.attack()
+    }
+
+    start() {
+        this.generateRevolver()
+    }
+
+    update(deltaTime: number) {
+        
+    }
+
+    generateRevolver(){
+        let revolver:Node = instantiate(this.revolverPrefab);
+        revolver.setPosition(this.node.position.x + 15, this.node.position.y - 15, this.node.position.z)
+        this.node.addChild(revolver)
+    }
+
+    attack() {
+        this.schedule(function() {
+            // this._attackMethod()
+            this.fire()
+        }, this._interval);
+    }
+
+    fire(){
+        //新的子弹生成新的预制体
+        let bullet:Node = instantiate(this.bulletPrefab);
+        
+        this.node.addChild(bullet);
+        // 注意，因为 Vec3 的计算方法都会修改自己的值，所以要先 clone 一个值再操作，避免修改到原始值
+        var posOffset = this.node.parent.getComponent(heroControl).posOffset.clone();
+        //子弹图层设置等于父节点图层
+       // bullet.layer = this.node.layer;
+        //设置相对父节点位置
+        let position = this.node.getChildByName("revolver").position
+        bullet.setPosition(position.x + 10, position.y + 5, position.z);
+
+        
+        // 初始化子弹的移动速度，这包括的是子弹的方向和速度
+        // console.log("xxx", bullet.getComponent(bulletControl));
+        
+        bullet.getComponent(bulletControl).posOffset = posOffset.multiplyScalar(5);
+
+        //挂载到炮台节点下
+        this.node.addChild(bullet);
+    }
+}
+
+
