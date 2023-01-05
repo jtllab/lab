4af2f561359573080e82cb85766c32c9bd4cac91
zuan <sevenneil@foxmail.com>
2023-01-05 20:28:45 +0800

    修复闪电

--- prod/BulletAgent/assets/script/hero/heroControl.ts
+++ prod/BulletAgent/assets/script/hero/heroControl.ts
@@ -5,6 +5,7 @@ import { rocketControl } from '../weapons/rocketControl';
 import { expControl } from '../exp/expControl';
 import { expMidControl } from '../exp/expMidControl';
 import { expBigControl } from '../exp/expBigControl';
+import { enemyBorn } from '../enemy/enemyBorn';
 const { ccclass, property } = _decorator;
 
 
@@ -346,15 +347,23 @@ export class heroControl extends Component {
      
      // 闪电
      lightning() {
-        let thunder:Node = instantiate(this.thunderPrefab);
         let parent = this.node.parent;
-        parent.addChild(thunder);
         
         let visibleSize = View.instance.getVisibleSize();
         let pos = this.playerMoveNode.worldPosition;
-        let enemiesInView : Array<Node> = new Array<Node>(); 
 
-        parent.children.forEach(c => {
+        let enemyBornNode:Node = null;
+        for (const c of parent.children) {
+            if (c.getComponent(enemyBorn)){
+                enemyBornNode = c;
+                break;
+            }
+        }
+
+        // 获取视野内敌人
+        let enemiesInView : Array<Node> = new Array<Node>(); 
+        
+        enemyBornNode.children.forEach(c => {
             if (c.getComponent(enemyControl)){
                 let c_pos = c.getWorldPosition();
                 let distance = new Vec3();
@@ -368,8 +377,12 @@ export class heroControl extends Component {
             }
         });
 
-        
+
+        // 找到最近的敌人并攻击
         if (enemiesInView.length > 0) {
+            let thunder:Node = instantiate(this.thunderPrefab);
+            parent.addChild(thunder);
+
             let nearestEnemy:Node = enemiesInView[0];
             let minPos  = new Vec3();
             Vec3.subtract(minPos, pos, nearestEnemy.worldPosition);
