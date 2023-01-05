599477a5921cc5e45c991c7ac6412d81b67669cc
zuan <sevenneil@foxmail.com>
2023-01-05 21:24:12 +0800

    闪电从herocontrol分离出来

--- prod/BulletAgent/assets/script/hero/heroControl.ts
+++ prod/BulletAgent/assets/script/hero/heroControl.ts
@@ -7,6 +7,7 @@ import { expMidControl } from '../exp/expMidControl';
 import { expBigControl } from '../exp/expBigControl';
 import { enemyBorn } from '../enemy/enemyBorn';
 import { guardianControl } from '../weapons/guardianControl';
+import { Thunder } from '../skill/Thunder';
 const { ccclass, property } = _decorator;
 
 
@@ -31,6 +32,9 @@ export class heroControl extends Component {
     rocketSW : boolean = false;
     thunderSW : boolean = false;
     guardianSW : boolean = false;
+
+    // 闪电技能
+    private _thunder:Thunder = null;
 
     //用于挂在火箭预制体
     @property(Prefab)
@@ -141,6 +145,7 @@ export class heroControl extends Component {
         this._attackMethod =  this.rocketFire;
 
         this.createGurdian();
+        this._thunder = new Thunder(this.node.parent, this.playerMoveNode, this.thunderPrefab);
 
         this.attack();
         this.collider = this.playerMoveNode.getComponent(BoxCollider2D);
@@ -266,7 +271,7 @@ export class heroControl extends Component {
                 break;
 
             case KeyCode.KEY_V:
-                this.lightning();
+                this._thunder.doSkill();
                 break;
         }
         this.updatePosOffset();
@@ -346,82 +351,6 @@ export class heroControl extends Component {
          //挂载到炮台节点下
          //this.node.parent.addChild(rocket);
      }
-
-     
-     // 闪电
-     lightning() {
-        let parent = this.node.parent;
-        
-        let visibleSize = View.instance.getVisibleSize();
-        let pos = this.playerMoveNode.worldPosition;
-
-        let enemyBornNode:Node = null;
-        for (const c of parent.children) {
-            if (c.getComponent(enemyBorn)){
-                enemyBornNode = c;
-                break;
-            }
-        }
-
-        // 获取视野内敌人
-        let enemiesInView : Array<Node> = new Array<Node>(); 
-        
-        enemyBornNode.children.forEach(c => {
-            if (c.getComponent(enemyControl)){
-                let c_pos = c.getWorldPosition();
-                let distance = new Vec3();
-                Vec3.subtract(distance, pos, c_pos);
-                distance.x = Math.abs(distance.x);
-                distance.y = Math.abs(distance.y);
-
-                if (distance.x <= visibleSize.x/2 && distance.y <= visibleSize.y/2){
-                    enemiesInView.push(c)
-                }
-            }
-        });
-
-
-        // 找到最近的敌人并攻击
-        if (enemiesInView.length > 0) {
-            let thunder:Node = instantiate(this.thunderPrefab);
-            parent.addChild(thunder);
-
-            let nearestEnemy:Node = enemiesInView[0];
-            let minPos  = new Vec3();
-            Vec3.subtract(minPos, pos, nearestEnemy.worldPosition);
-
-            enemiesInView.forEach(e => {
-                let e_pos = e.getWorldPosition();
-                let distance = new Vec3();
-                Vec3.subtract(distance, pos, e_pos);
-
-                if (Vec3.len(distance) < Vec3.len(minPos)) {
-                    minPos = distance;
-                    nearestEnemy = e;
-                }
-            });
-            let thunderAni = thunder.getComponent(Animation);
-            thunder.setWorldPosition(nearestEnemy.worldPosition);
-            thunderAni.play();
-
-            // 杀伤范围内敌人
-            let radius = 100;
-            enemiesInView.forEach(e => {
-                let distance = new Vec3();
-                let e_pos = e.getWorldPosition();
-                Vec3.subtract(distance, e_pos, nearestEnemy.worldPosition);
-
-                if (Vec3.len(distance) < radius) {
-                    // 敌人扣血
-                    // e.getComponent(enemyControl)
-                }
-            });
-
-            this.scheduleOnce(() => {
-                thunder.destroy();
-            }, 0.5);
-        }
-    }
 
      //生成守护者
      createGurdian()
