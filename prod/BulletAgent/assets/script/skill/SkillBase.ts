35e6beca5c1659a1b695aeaf98ad763358690811
zuan <sevenneil@foxmail.com>
2023-01-05 21:19:30 +0800

    分离闪电技能为单独脚本

---
+++ prod/BulletAgent/assets/script/skill/SkillBase.ts
@@ -0,0 +1,18 @@
+import { _decorator, Component, Node, Prefab } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('SkillBase')
+export class SkillBase extends Component {
+    protected _parent : Node = null;
+    protected _hero : Node = null;
+    protected _prefab : Prefab = null;
+
+    constructor(parent:Node, hero:Node, prefab:Prefab){
+        super();
+        this._parent = parent;
+        this._hero = hero;
+        this._prefab = prefab;
+    }
+}
+
+
---
+++ prod/BulletAgent/assets/script/skill/Thunder.ts
@@ -0,0 +1,82 @@
+import { _decorator, Component, instantiate, Node, Prefab, Animation, Vec3, Scheduler , View } from 'cc';
+import { enemyBorn } from '../enemy/enemyBorn';
+import { enemyControl } from '../enemy/enemyControl';
+import { SkillBase } from './SkillBase';
+const { ccclass, property } = _decorator;
+
+@ccclass('Thunder')
+export class Thunder extends SkillBase {
+    doSkill() {
+        let visibleSize = View.instance.getVisibleSize();
+        let pos = this._hero.worldPosition;
+
+        let enemyBornNode:Node = null;
+        for (const c of this._parent.children) {
+            if (c.getComponent(enemyBorn)){
+                enemyBornNode = c;
+                break;
+            }
+        }
+
+        // 获取视野内敌人
+        let enemiesInView : Array<Node> = new Array<Node>(); 
+        
+        enemyBornNode.children.forEach(c => {
+            if (c.getComponent(enemyControl)){
+                let c_pos = c.getWorldPosition();
+                let distance = new Vec3();
+                Vec3.subtract(distance, pos, c_pos);
+                distance.x = Math.abs(distance.x);
+                distance.y = Math.abs(distance.y);
+
+                if (distance.x <= visibleSize.x/2 && distance.y <= visibleSize.y/2){
+                    enemiesInView.push(c)
+                }
+            }
+        });
+
+
+        // 找到最近的敌人并攻击
+        if (enemiesInView.length > 0) {
+            let thunder:Node = instantiate(this._prefab);
+            this._parent.addChild(thunder);
+
+            let nearestEnemy:Node = enemiesInView[0];
+            let minPos  = new Vec3();
+            Vec3.subtract(minPos, pos, nearestEnemy.worldPosition);
+
+            enemiesInView.forEach(e => {
+                let e_pos = e.getWorldPosition();
+                let distance = new Vec3();
+                Vec3.subtract(distance, pos, e_pos);
+
+                if (Vec3.len(distance) < Vec3.len(minPos)) {
+                    minPos = distance;
+                    nearestEnemy = e;
+                }
+            });
+            let thunderAni = thunder.getComponent(Animation);
+            thunder.setWorldPosition(nearestEnemy.worldPosition);
+            thunderAni.play();
+
+            // 杀伤范围内敌人
+            let radius = 100;
+            enemiesInView.forEach(e => {
+                let distance = new Vec3();
+                let e_pos = e.getWorldPosition();
+                Vec3.subtract(distance, e_pos, nearestEnemy.worldPosition);
+
+                if (Vec3.len(distance) < radius) {
+                    // 敌人扣血
+                    // e.getComponent(enemyControl)
+                }
+            });
+
+            this.scheduleOnce(() => {
+                thunder.destroy();
+            }, 0.5);
+        }
+    }
+}
+
+
