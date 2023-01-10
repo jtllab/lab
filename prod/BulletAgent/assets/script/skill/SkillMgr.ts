a7eda0ebd3293f2415e720d5772fcb40026b7579
ZnBDPang <gldzpang@outlook.com>
2023-01-10 22:01:56 +0800

    生成无人机准星

--- prod/BulletAgent/assets/script/skill/SkillMgr.ts
+++ prod/BulletAgent/assets/script/skill/SkillMgr.ts
@@ -21,7 +21,7 @@ export class SkillMgr extends Component {
     guardianPrefab : Prefab = null;
 
     @property(Prefab)
-    drone : Prefab = null;
+    dronePrefab : Prefab = null;
 
     // 技能
     private _thuder: SkillBase = null;
@@ -47,6 +47,7 @@ export class SkillMgr extends Component {
         root.addComponent(Thunder);
         root.addComponent(Rocket);
         root.addComponent(Guardian);
+        root.addComponent(Drone);
 
         //结点加到场景
         this.node.addChild(root);
@@ -61,9 +62,13 @@ export class SkillMgr extends Component {
         this._guardian = root.getComponent(Guardian);
         this._guardian.init(canvas, heroNode, this.guardianPrefab);
 
+        this._drone = root.getComponent(Drone);
+        this._drone.init(canvas, heroNode, this.dronePrefab);
+
         this._skillArr.push(this._thuder);
         this._skillArr.push(this._rocket);
         this._skillArr.push(this._guardian);
+        this._skillArr.push(this._drone);
 
 
         this.doSkillRepeat();
